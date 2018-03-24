1eaac0a19e04b5ee861bc3a14dfceb9c5cabb340
Victor Glindås <glinkis@gmail.com>
2018-03-24 13:16:50 +0100

    Refactors OrbitalCamera.

--- src/objects/OrbitalCamera.ts
+++ src/objects/OrbitalCamera.ts
@@ -14,17 +14,17 @@ const MAGNITUDE_PANNING = 0.004;
 const MAGNITUDE_ZOOM = 0.004;
 
 export default class OrbitalCamera extends PerspectiveCamera {
-  public distance: number;
+  public distance = 100;
 
   private eventElement?: HTMLElement;
-  private centerPoint: Vector3;
-  private rotationX: number;
-  private rotationY: number;
-  private panX: number;
-  private panY: number;
+  private centerPoint = new Vector3();
+  private rotationX = 0;
+  private rotationY = 0;
+  private panX = 0;
+  private panY = 0;
   private zoomPvt: number;
-  private zoomMin: number;
-  private zoomMax: number;
+  private zoomMin = 1;
+  private zoomMax = 4;
 
   get zoom() {
     return this.zoomPvt;
@@ -36,23 +36,7 @@ export default class OrbitalCamera extends PerspectiveCamera {
 
   constructor() {
     super();
-
-    this.distance = 100;
-    this.centerPoint = new Vector3();
-
-    this.rotationX = 0;
-    this.rotationY = 0;
-
-    this.panX = 0;
-    this.panY = 0;
-
-    this.zoomMax = 4;
-    this.zoomMin = 1;
-
     this.update();
-
-    this.onMouseWheel = this.onMouseWheel.bind(this);
-    this.onStartEvent = this.onStartEvent.bind(this);
   }
 
   public update() {
@@ -85,7 +69,7 @@ export default class OrbitalCamera extends PerspectiveCamera {
     this.eventElement = element;
   }
 
-  private onMouseWheel(event: WheelEvent) {
+  private onMouseWheel = (event: WheelEvent) => {
     event.preventDefault();
     let delta = 0;
     if (event.wheelDelta) {
@@ -99,9 +83,9 @@ export default class OrbitalCamera extends PerspectiveCamera {
     }
     this.zoom += delta * MAGNITUDE_ZOOM;
     this.updateProjectionMatrix();
-  }
+  };
 
-  private onStartEvent(startEvent: any) {
+  private onStartEvent = (startEvent: any) => {
     startEvent.preventDefault();
 
     const start = eventByType(startEvent);
@@ -118,29 +102,27 @@ export default class OrbitalCamera extends PerspectiveCamera {
 
     const onMoveEvent = (moveEvent: any) => {
       const move = eventByType(moveEvent);
-      const numberOfTouches = moveEvent.touches
-        ? moveEvent.touches.length
-        : false;
+      const touches = moveEvent.touches ? moveEvent.touches.length : 0;
       const movementDelta = eventDeltaMovement(start, move);
 
-      if (numberOfTouches === 2) {
-        const touchMovementDelta = touchesDeltaDistance(
-          startEvent.touches,
-          moveEvent.touches
+      if (touches === 2) {
+        const delta = touchesDeltaDistance(
+          moveEvent.touches[0],
+          moveEvent.touches[1]
         );
-        this.zoom = zoomCompound - touchMovementDelta * MAGNITUDE_ZOOM;
+        this.zoom = zoomCompound - delta * MAGNITUDE_ZOOM;
       }
 
-      if (numberOfTouches === 2 || moveEvent.button === 1) {
+      if (touches === 2 || moveEvent.button === 1) {
         this.panX = panCompoundX + movementDelta.x * MAGNITUDE_PANNING * 2;
         this.panY = panCompoundY + movementDelta.y * MAGNITUDE_PANNING * 2;
       }
 
-      if (numberOfTouches === 1 || moveEvent.button === 0) {
-        this.rotationX =
-          rotationCompoundX + movementDelta.x * MAGNITUDE_ROTATION;
-        this.rotationY =
-          rotationCompoundY + movementDelta.y * MAGNITUDE_ROTATION;
+      if (touches === 1 || moveEvent.button === 0) {
+        this.rotationX = rotationCompoundX;
+        this.rotationX += movementDelta.x * MAGNITUDE_ROTATION;
+        this.rotationY = rotationCompoundY;
+        this.rotationY += movementDelta.y * MAGNITUDE_ROTATION;
       }
 
       this.update();
@@ -153,7 +135,7 @@ export default class OrbitalCamera extends PerspectiveCamera {
 
     document.addEventListener(moveType, onMoveEvent);
     document.addEventListener(endType, onEventEnd);
-  }
+  };
 
   private correctCameraUpVector() {
     this.up = this.centerPoint.clone();
