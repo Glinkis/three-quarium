357ff1c42c924db2b61a54e10fc575dba24f05be
Victor Glindås <glinkis@gmail.com>
2018-04-09 21:24:26 +0200

    Updates dependencies.

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,42 @@ import Simulation from "./objects/Simulation";
 import UIButton from "./ui/UIButton";
 import UIGroup from "./ui/UIGroup";
 
-const simulation = new Simulation();
-simulation.scene.add(new Bounds(simulation.size));
+function initialize() {
+  const simulation = new Simulation();
+  simulation.scene.add(new Bounds(simulation.size));
 
-const uiGroup = new UIGroup(simulation.element);
+  const uiGroup = new UIGroup(simulation.element);
 
-const btnAddOne = new UIButton(uiGroup.element);
-btnAddOne.element.textContent = "Add 1";
-btnAddOne.onClick = () => {
-  simulation.scene.add(new Organism(simulation));
-};
-
-const btnAddTen = new UIButton(uiGroup.element);
-btnAddTen.element.textContent = "Add 10";
-btnAddTen.onClick = () => {
-  for (let i = 0; i < 10; i++) {
+  const btnAddOne = new UIButton(uiGroup.element);
+  btnAddOne.element.textContent = "Add 1";
+  btnAddOne.onClick = () => {
     simulation.scene.add(new Organism(simulation));
-  }
-};
+  };
 
-const btnClearAll = new UIButton(uiGroup.element);
-btnClearAll.element.textContent = "Clear All";
-btnClearAll.onClick = () => {
-  const organisms = [];
-
-  simulation.scene.traverse(child => {
-    if (child instanceof Organism) {
-      organisms.push(child);
+  const btnAddTen = new UIButton(uiGroup.element);
+  btnAddTen.element.textContent = "Add 10";
+  btnAddTen.onClick = () => {
+    for (let i = 0; i < 10; i++) {
+      simulation.scene.add(new Organism(simulation));
     }
-  });
+  };
 
-  organisms.forEach(organism => {
-    simulation.scene.remove(organism);
-    simulation.scene.remove(organism.trail);
-  });
-};
+  const btnClearAll = new UIButton(uiGroup.element);
+  btnClearAll.element.textContent = "Clear All";
+  btnClearAll.onClick = () => {
+    const organisms = [];
+
+    simulation.scene.traverse(child => {
+      if (child instanceof Organism) {
+        organisms.push(child);
+      }
+    });
+
+    organisms.forEach(organism => {
+      simulation.scene.remove(organism);
+      simulation.scene.remove(organism.trail);
+    });
+  };
+}
+
+window.addEventListener("load", initialize);
