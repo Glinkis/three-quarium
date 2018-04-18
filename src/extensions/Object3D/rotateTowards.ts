5888638e035899a4b0e29ca4f61072d2158ae3b6
Victor Glindås <glinkis@gmail.com>
2018-04-18 20:35:33 +0200

    Better naming.

--- src/extensions/Object3D/rotateTowards.ts
+++ src/extensions/Object3D/rotateTowards.ts
@@ -1,7 +1,7 @@
 import { Matrix4, Object3D, Quaternion, Vector3 } from "three";
 
 export default (object: Object3D, target: Vector3, amount: number) => {
-  const matrix = new Matrix4().lookAt(target, object.position, object.up);
-  const qt = new Quaternion().setFromRotationMatrix(matrix);
-  object.quaternion.slerp(qt, amount).normalize();
+  const direction = new Matrix4().lookAt(target, object.position, object.up);
+  const rotation = new Quaternion().setFromRotationMatrix(direction);
+  object.quaternion.slerp(rotation, amount).normalize();
 };
