7128be327adee5cd6921d98d54425f7ea709a8c4
Victor Glindås <glinkis@gmail.com>
2018-04-20 21:59:50 +0200

    Convert TrailGeometry to BufferGeoometry.

--- src/objects/TrailGeometry.ts
+++ src/objects/TrailGeometry.ts
@@ -1,16 +1,46 @@
-import { Geometry, Vector3 } from "three";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Float32BufferAttribute,
+  Vector3
+} from "three";
 
-export default class TrailGeometry extends Geometry {
+export default class TrailGeometry extends BufferGeometry {
   constructor(target: Vector3) {
     super();
-    this.vertices.length = 32;
-    this.vertices.fill(target.clone());
+    this.addPosition(target);
   }
 
   public update(target: Vector3) {
-    const vertex = target.clone();
-    this.vertices.shift();
-    this.vertices.push(vertex);
-    this.verticesNeedUpdate = true;
+    this.updatePosition(target);
+  }
+
+  private addPosition(target: Vector3) {
+    const vertices: number[] = [];
+    for (let i = 0; i < 32; i++) {
+      vertices.push(...target.toArray());
+    }
+    this.addAttribute("position", new Float32BufferAttribute(vertices, 3));
+  }
+
+  private updatePosition(target: Vector3) {
+    const position = this.getAttribute("position");
+
+    const array = position.array;
+
+    if (array instanceof Float32Array) {
+      const current = new Float32Array(3);
+      const last = new Float32Array(target.toArray());
+
+      for (let i = 0; i < array.length; i += 3) {
+        current.set(array.subarray(i, i + 3));
+        array.set(last, i);
+        last.set(current);
+      }
+    }
+
+    if (position instanceof BufferAttribute) {
+      position.needsUpdate = true;
+    }
   }
 }
