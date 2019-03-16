9445d71571667c742cbc132901f43dfe339cac42
Victor Glindås <glinkis@gmail.com>
2019-03-16 22:30:22 +0100

    Refactor clamp function

--- src/math/clamp.ts
+++ src/math/clamp.ts
@@ -1,5 +1,5 @@
 export default (value: number, min: number, max: number) => {
-  value = max !== 0 && value >= max ? max : value;
-  value = min !== 0 && value <= min ? min : value;
+  value = value > max ? max : value;
+  value = value < min ? min : value;
   return value;
 };
